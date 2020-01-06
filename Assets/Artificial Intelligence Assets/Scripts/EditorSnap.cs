65815e6a2041ded0edf2c05aa10301ec8f87ac01
purplemoonboi <47715008+purplemoonboi@users.noreply.github.com>
2020-01-06 23:53:47 +0000

    AI Scene
    Created an AI scene for AI development. i.e currently working on A* pathfinding.

---
+++ Assets/Artificial Intelligence Assets/Scripts/EditorSnap.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+public class EditorSnap : MonoBehaviour
+{
+
+    [Header("Grid Size")] [SerializeField] [Range(10,100)] float gridSize = 10f;
+
+    TextMesh textMesh;
+
+    private void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        Vector3 snapPos;
+
+        snapPos.x = Mathf.RoundToInt(transform.position.x /gridSize) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+
+        string labelText = snapPos.x / gridSize + ", " + snapPos.z / gridSize;
+        textMesh.text = labelText;
+        gameObject.name = labelText;
+    }
+}
---
+++ Assets/Artificial Intelligence Assets/Scripts/EnemyMover.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+
+    [SerializeField] List<Waypoint> path;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FollowPath());
+    }
+
+
+    IEnumerator FollowPath()
+    {
+        foreach(Waypoint waypoint in path)
+        {
+            transform.position = waypoint.transform.position;
+           Vector3 currentPos = new Vector3(transform.position.x, 0, transform.position.z);
+
+            yield return new WaitForSeconds(1f);
+
+            Vector3 finalPos = new Vector3(3, 0, 1);
+            if(finalPos.x == 3 && finalPos.z == 1)
+            {
+                StartCoroutine(FollowPath());
+            }
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
