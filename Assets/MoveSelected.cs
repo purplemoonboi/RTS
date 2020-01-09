3810a0507824658113929131b6533d5ecea64cc8
purplemoonboi <47715008+purplemoonboi@users.noreply.github.com>
2020-01-09 20:09:31 +0000

    Player AI/Nav mesh
    Started writing Player movement script based on unitys nav mesh.

---
+++ Assets/MoveSelected.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class MoveSelected : MonoBehaviour
+{
+    [SerializeField] Transform targetLocation;
+    NavMeshAgent navMeshAgent;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.destination = targetLocation.position;
+    }
+
+    void SelectGameObject()
+    {
+        //select a game obj to manipulate.
+    }
+
+    void SetTargetPosition()
+    {
+        //set the loc for the game obj to move to.
+    }
+}
